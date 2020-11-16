60edca662704f4432aa897e680a1a721b2e63b12
fshams <faraz.shams@hyperanna.com>
2020-11-16 16:38:11 +1100

    first commit

---
+++ src/components/InputTodo.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+    state = {
+        title: ""
+    };
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addTodoProps(this.state.title);
+        this.setState({
+            title: ""
+        });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="form-container">
+                <input type="text" placeholder="Add Todo..." value={this.state.title}
+                       className="input-text" name="title" onChange={this.onChange}/>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+export default InputTodo
